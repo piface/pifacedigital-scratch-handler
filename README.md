0c735cbd8832230eda83f6c45c16eccdc2589906
Thomas Preston <thomasmarkpreston@gmail.com>
2013-03-20 21:39:34 +0000

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,34 @@ pifacedigital-scratch-handler
 =============================
 
 Allows Scratch to control PiFace Digital through MESH.
+
+Depends on [pifacedigitalio](https://github.com/piface/pifacedigitalio).
+
+Usage
+=====
+First you must [enable Mesh in
+Scratch](http://wiki.scratch.mit.edu/wiki/Mesh#Mesh_by_Modification_of_Scratch).
+
+The shift-click the *Share* menu-item and select *Host Mesh*.
+
+Now run the scratch\_handler: 
+
+    $ python3 scratch_handler.py
+
+Use the **-e** flag to use the scratch\_handler with the PiFace Digital Emulator
+instead of PiFace Digital (if you have it installed).
+
+Scratch
+-------
+The handler will make available inputs 1 through 8 via the sensor values. If
+you want to control PiFace Digital's outputs you must create a variable for
+each pin named like so:
+
+    piface-output1
+    piface-output2
+    piface-output3
+    piface-output4
+    piface-output5
+    piface-output6
+    piface-output7
+    piface-output8
---
+++ scratch_handler.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python3
+"""scratch_handler
+Scratch Handler allows Scratch to talk to PiFace digital through MESH
+Copyright (C) 2013 Thomas Preston (thomasmarkpreston@gmail.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+from array import array
+from time import sleep
+import threading
+import socket
+import sys
+import struct
+
+if "-e" in sys.argv:
+    import pifacedigital_emulator as pfio
+    sys.argv.remove("-e")
+else:
+    import pifacedigitalio as pfio
+
+PORT = 42001
+DEFAULT_HOST = '127.0.0.1'
+BUFFER_SIZE = 100
+SOCKET_TIMEOUT = 1
+
+SCRATCH_SENSOR_NAME_INPUT = (
+    'piface-input1',
+    'piface-input2',
+    'piface-input3',
+    'piface-input4',
+    'piface-input5',
+    'piface-input6',
+    'piface-input7',
+    'piface-input8')
+
+SCRATCH_SENSOR_NAME_OUTPUT = (
+    'piface-output1',
+    'piface-output2',
+    'piface-output3',
+    'piface-output4',
+    'piface-output5',
+    'piface-output6',
+    'piface-output7',
+    'piface-output8')
+
+
+class ScratchListener(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+        self.last_zero_bit_mask = 0
+        self.last_one_bit_mask  = 0
+
+    def stop(self):
+        self.alive = False
+
+    def run(self):
+        self.alive = True
+        while self.alive:
+            try:
+                global scratch_socket
+                data = scratch_socket.recv(BUFFER_SIZE).decode('utf-8')
+                data = data[4:] # get rid of the length info
+
+            except socket.timeout: # if we timeout, re-loop
+                continue
+            except: # exit on any other errrors
+                break
+
+            data = data.split(" ")
+            
+            if data[0] == 'sensor-update':
+                data = data[1:]
+                print('received sensor-update:', data)
+                self.sensor_update(data)
+                    
+            elif data[0] == 'broadcast':
+                data = data[1:]
+                print('received broadcast:', data)
+
+            else:
+                print('received something:', data)
+
+    def sensor_update(self, data):
+        index_is_data = False # ignore the loop contents if not sensor
+        zero_bit_mask = 0 # bit mask showing where zeros should be written
+        one_bit_mask  = 0 # bit mask showing where ones should be written
+        we_should_update_piface = False
+
+        # go through all of the sensors that have been updated
+        for i in range(len(data)):
+            if index_is_data:
+                index_is_data = False
+                continue
+
+            sensor_name = data[i].strip('"')
+            
+            # if this sensor is a piface output then reflect
+            # that update on the board
+            if sensor_name in SCRATCH_SENSOR_NAME_OUTPUT:
+                we_should_update_piface = True
+                pin_index = SCRATCH_SENSOR_NAME_OUTPUT.index(sensor_name)
+                sensor_value = int(data[i+1])
+                index_is_data = True
+
+                # could this be made more efficient by sending a single write
+                if sensor_value == 0:
+                    zero_bit_mask ^= (1 << pin_index)
+                else:
+                    one_bit_mask ^= (1 << pin_index)
+
+        if we_should_update_piface:
+            old_pin_bitp = pfio.read(pfio.OUTPUT_PORT) # grab the old values
+            new_pin_bitp = old_pin_bitp & ~zero_bit_mask # set the zeros
+            new_pin_bitp |= one_bit_mask # set the ones
+
+            if new_pin_bitp != old_pin_bitp:
+                pfio.write(pfio.OUTPUT_PORT, new_pin_bitp) # write the new bit pattern
+
+
+def input_handler(interrupted_bit, input_byte):
+    """Callback function for when inputs are changed"""
+    pin_num = pfio.get_bit_num(interrupted_bit)
+    value   = (interrupted_bit & input_byte) >> pin_num
+    broadcast_pin_update(pin_num, value^1) # flip the bit (inputs: active low)
+
+def broadcast_pin_update(pin_index, value):
+    sensor_name = SCRATCH_SENSOR_NAME_INPUT[pin_index]
+    bcast_str = 'sensor-update "%s" %d' % (sensor_name, value)
+    print('sending:', bcast_str)
+    send_scratch_command(bcast_str)
+
+def send_scratch_command(cmd):
+    global scratch_socket
+    length = len(cmd).to_bytes(4, byteorder='big')
+    scratch_socket.send(length + cmd.encode("utf-8"))
+
+def create_socket(host, port):
+    try:
+        scratch_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        scratch_sock.connect((host, port))
+    except socket.error:
+        print("There was an error connecting to Scratch!")
+        print("Could not find MESH session at %s:%s" % (host, port))
+        sys.exit(1)
+
+    return scratch_sock
+
+if __name__ == '__main__':
+    # has the hostname been given?
+    host = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_HOST
+
+    # setup the socket
+    print('Connecting...' , end=" ")
+    global scratch_socket
+    scratch_socket = create_socket(host, PORT)
+    print('Connected.')
+
+    scratch_socket.settimeout(SOCKET_TIMEOUT)
+
+    pfio.init()
+
+    # hook each input to the callback function
+    ifm = pfio.InputFunctionMap()
+    for i, sensor in enumerate(SCRATCH_SENSOR_NAME_INPUT):
+        ifm.register(i, None, input_handler)
+        broadcast_pin_update(i, 0) # make scratch is aware of the input pins
+
+    # start another thread to handle scratch stuff
+    listener = ScratchListener()
+    listener.start()
+
+    try:
+        # if an input is pressed, update scratch
+        pfio.wait_for_input(input_func_map=ifm, loop=True)
+    except KeyboardInterrupt:
+        pass
+
+    print("Stopping...", end=" ")
+    listener.stop()
+    listener.join()
+    pfio.deinit()
+    print("Done.")
